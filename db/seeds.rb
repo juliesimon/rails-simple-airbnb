5a89a431c873f333d5e1bbdc782c97f70d0bda18
Julie Simon <juliesimon.js@gmail.com>
2017-11-23 09:33:53 +0100

    almost nothing coded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Crappy flat in London',
+    address: '10 High Street London W3 1DT',
+    description: 'A disgusting flat in one of the worst place in whole London. I would never leave there. But it\'s cheap.',
+    price_per_night: 10,
+    number_of_guests: 1
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
